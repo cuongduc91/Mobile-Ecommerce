193cef0a0cc3e1f61606d6b1dc58267d5c2c79f6
Cuong <bdcuong91@gmail.com>
2020-06-12 17:00:03 +0200

    add isotope to sort the products

--- src/index.js
+++ src/index.js
@@ -21,4 +21,17 @@ $(document).ready(function () {
       }
     }
   })
+  // isotope filter
+  var $grid = $(".grid").isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  })
+  //filter on button click
+  $(".button-group").on("click", "button", function () {
+    var filterValue = $(this).attr("data-filter");
+    $grid.isotope({
+      filter: filterValue
+    });
+  })
+
 })
